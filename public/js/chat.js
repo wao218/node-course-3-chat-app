83d8f4bd2eb06f50beaeed0ae353ef6932cb4adc
Wesley Osborne <wao218@lehigh.edu>
2020-02-18 18:06:56 -0500

    Added geolocation events between clients

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,4 +9,18 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
   const message = e.target.elements.message.value;
   socket.emit('sendMessage', message);
-})
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+      return alert('Geolocation is not supported by your browser.')
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+      socket.emit('sendLocation', {
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+      });
+      
+  });
+});

8100ebb99fdfd1101bb824aa02f6adc271251776
Wesley Osborne <wao218@lehigh.edu>
2020-02-18 06:59:10 -0500

    Added socket events for messages

--- src/index.js
+++ src/index.js
@@ -12,8 +12,15 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+
+io.on('connection', (socket) => {
   console.log('New WebSocket Connection'); 
+
+  socket.emit('message', 'Welcome!');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message ', message);
+  });
 });
 
 server.listen(port, () => {

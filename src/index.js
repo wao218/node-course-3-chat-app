dc85f580a5b3db3355e371b5d044521b95783e15
Wesley Osborne <wao218@lehigh.edu>
2020-02-18 17:21:03 -0500

    Added events for when a new client connects and disconnects

--- src/index.js
+++ src/index.js
@@ -12,17 +12,21 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-
 io.on('connection', (socket) => {
-  console.log('New WebSocket Connection'); 
+    console.log('New WebSocket connection');
 
-  socket.emit('message', 'Welcome!');
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
 
-  socket.on('sendMessage', (message) => {
-    io.emit('message ', message);
-  });
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+      io.emit('message', 'A user has left!');
+    });
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
+    console.log(`Server is up on port ${port}!`);
 });

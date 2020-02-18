1fbb231209e965abdd65ec28c8b6bb442c3e3234
Wesley Osborne <wao218@lehigh.edu>
2020-02-18 18:42:36 -0500

    Added event acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,19 +8,28 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
   e.preventDefault();
 
   const message = e.target.elements.message.value;
-  socket.emit('sendMessage', message);
+
+  socket.emit('sendMessage', message, (error) => {
+    if (error) {
+      console.log(error);
+    }
+
+    console.log('Message delivered!'); 
+  });
 });
 
 document.querySelector('#send-location').addEventListener('click', () => {
   if (!navigator.geolocation) {
-      return alert('Geolocation is not supported by your browser.')
+    return alert('Geolocation is not supported by your browser.')
   }
 
   navigator.geolocation.getCurrentPosition((position) => {
-      socket.emit('sendLocation', {
-        lat: position.coords.latitude,
-        long: position.coords.longitude
-      });
-      
+    socket.emit('sendLocation', {
+      lat: position.coords.latitude,
+      long: position.coords.longitude
+    }, (message) => {
+      console.log('Location shared!')
+    });
+
   });
 });

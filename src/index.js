d997e5e8c5d0f0e44fa40217185d768595302c83
Wesley Osborne <wao218@lehigh.edu>
2020-02-19 22:32:13 -0500

    Added timestamps to location message

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
-const { generateMessage } = require('./utils/messages');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('sendLocation', (coords, callback) => {
-      io.emit('locationMessage', `https://google.com/maps?q=${coords.lat},${coords.long}`);
+      io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.lat},${coords.long}`));
       callback();
     });
 
